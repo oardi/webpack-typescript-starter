d0fc10c89c3e47ad7ef595829c40b00acabbfc22
Ardian Shala <ardian.sh@outlook.de>
2022-06-29 11:07:11 +0300

    init

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	context: path.resolve(__dirname, './src'),
+	entry: { app: './index.ts' },
+	module: {
+		rules: [{ test: /\.ts?$/, use: 'ts-loader', exclude: /node_modules/ }], // do not forget to change/install your own TS loader
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	plugins: [
+		new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, "src", "index.html"),
+			title: 'Webpack Typescript Starter',
+			filename: "index.html",
+			chunks: ['vendors', 'app'],
+		})
+	],
+	output: {
+		filename: '[name].js',
+		chunkFilename: '[name].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+};
